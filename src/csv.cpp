b00365a779a174573bd87cf061181c9c787948dd
Luis Panadero Guardeño <luis.panadero@gmail.com>
2021-10-24 19:01:20 +0200

    GMXEXT_mod_csv: ERROR: Invalid timing value (must be 0 or 1)

--- src/csv.cpp
+++ src/csv.cpp
@@ -13,10 +13,10 @@ GMXDEFINE_LIBRARY_PROPERTIES(GMXEXT_mod_csv,
 
 GMXDEFINE_LIBRARY_EXPORTS(GMXEXT_mod_csv,
   GMXDEFINE_FUNCTIONS(
-      "csv_readtoarray(S,I,I8P=0)",  "I", 1, GMXEXT_CSV_readToInt8Array,
-    //  "csv_readtoarray(S,I,I16P=0)", "I", 1, GMXEXT_CSV_readToInt16Array,
-      "csv_readtoarray(S,I,I32P=0)", "I", 1, GMXEXT_CSV_readToInt32Array
-    // "CSV_ReadToArray(S,I,U32P)", "I", 1, GMXEXT_CSV_readToUInt32Array
+      "CSV_ReadToArray(S,I,I8P)",  "I", 1, GMXEXT_CSV_readToInt8Array,
+    //"CSV_ReadToArray(S,I,I16P)", "I", 1, GMXEXT_CSV_readToInt16Array
+      "CSV_ReadToArray(S,I,I32P)", "I", 1, GMXEXT_CSV_readToInt32Array
+    //"CSV_ReadToArray(S,I,U32P)", "I", 1, GMXEXT_CSV_readToUInt32Array
   );
 );
 
